80c29c540ab666b05f3299fca89c1e6c895c398c
Unknown <Alee14498@gmail.com>
2017-08-21 21:37:00 -0400

    Added new files

--- Smart TV for Windows/Program.cs
+++ Smart TV for Windows/Program.cs
@@ -16,7 +16,7 @@ namespace Smart_TV_for_Windows
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new menu());
         }
     }
 }
--- Smart TV for Windows/netflix.cs
+++ Smart TV for Windows/netflix.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Smart_TV_for_Windows
 {
-    public partial class Form1 : Form
+    public partial class netflix : Form
     {
-        public Form1()
+        public netflix()
         {
             InitializeComponent();
         }
